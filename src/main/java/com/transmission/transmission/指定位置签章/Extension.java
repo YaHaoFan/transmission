7fcf47a94304d6a8f6848d6758e87c9c4e54d3bf
fanyahao <fanyahao123@vip.qq.com>
2020-12-25 09:09:07 +0800

    1111

---
+++ src/main/java/com/transmission/transmission/指定位置签章/Extension.java
@@ -0,0 +1,20 @@
+package com.transmission.transmission.指定位置签章;
+
+public class Extension {
+    private String oid;
+
+    private boolean critical;
+
+    private byte[] value;
+
+    public String getOid() {
+        return oid;
+    }
+
+    public byte[] getValue() {
+        return value;
+    }
+    public boolean isCritical() {
+        return critical;
+    }
+}
